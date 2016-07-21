593b7f133ac78a39c725d9a1b64cfe480ded4fd3
lilrichy <richardreigens@gmail.com>
2016-07-21 02:55:58 -0400

    Added varient for  Lamp Block & Glowstone Lamp Block for when they are placed looking up at a block. Added new models, Changed the Texture for the Glowstone lamp model.

--- src/main/java/lilrichy/lilrichymod/blocks/BlockLamp.java
+++ src/main/java/lilrichy/lilrichymod/blocks/BlockLamp.java
@@ -4,9 +4,15 @@ import lilrichy.lilrichymod.creativeTab.CreativeTabLiLRichyMod;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 
 import javax.annotation.Nonnull;
 
@@ -14,6 +20,8 @@ import javax.annotation.Nonnull;
  * Created by LiLRichy on 7/18/2016.
  */
 public class BlockLamp extends Block {
+
+    public static final PropertyBool isUp = PropertyBool.create("up");
 
     public BlockLamp(String unlocalizedName, String registryName) {
         super(Material.GLASS);
@@ -23,6 +31,38 @@ public class BlockLamp extends Block {
         this.setHardness(2f);
         this.setSoundType(SoundType.GLASS);
         this.setLightLevel(0.9375F);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(isUp, false));
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, isUp);
+    }
+
+    /**
+     * Convert the given metadata into a BlockState for this Block
+     */
+    public IBlockState getStateFromMeta(int meta) {
+        return this.getDefaultState().withProperty(isUp, (meta & 1) > 0);
+    }
+
+    /**
+     * Convert the BlockState into the correct metadata value
+     */
+    public int getMetaFromState(IBlockState state) {
+        if (state.getValue(isUp))
+            return 1;
+        else return 0;
+    }
+
+    @Override
+    public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+        IBlockState iblockstate = this.getDefaultState();
+
+        if (facing.equals(EnumFacing.DOWN))
+            return iblockstate.withProperty(isUp, true);
+        else
+            return iblockstate;
     }
 
     @Override
